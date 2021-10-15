b4d5e353ca22852f8dcb2d57950b0716e52a11a6
hnamzian <namazian87@gmail.com>
2021-10-15 20:33:10 +0300

    Solve meeting arrangement solution

---
+++ README.md
@@ -0,0 +1,13 @@
+Just to be funny :)
+
+**Problem**: Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei). 
+What is the min number of romms needed to schedule all the meetings?
+
+**Examples**:
+Input: [2, 10], [3, 4], [6, 9]
+Output: 2
+
+Input: [1, 3], [2, 4], [2, 5], [3, 4], [3, 6], [4, 5], [5, 6]
+Output: 4
+
+**Solution**: Create rooms list consisting of rooms of meetings. Each meeting is and array of 2 numbers (start and end). Start with an empty room list, will iteratively add meeting to a room which is free within the meeting period, else will create a new room and push the meeting there. 
---
+++ dist/meeting.js
@@ -0,0 +1,59 @@
+"use strict";
+class Greeter {
+    constructor(message) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+    arrangeRooms(meetings) {
+        let rooms = [];
+        for (const meeting of meetings) {
+            // if has interference with other rooms' events, add new room
+            const freeRoomId = this._hasFreeRoomForMeeting(meeting, rooms);
+            rooms = this._extendRoomMeetingTime(freeRoomId, rooms, meeting);
+        }
+        return rooms;
+    }
+    _hasInterferenceWithMeeting(newMeeting, refMeeting) {
+        if (newMeeting[0] >= refMeeting[1] || newMeeting[1] <= refMeeting[0]) {
+            return false;
+        }
+        return true;
+    }
+    _hasInterferenceWithRoom(meeting, room) {
+        for (const refMmeting of room) {
+            if (this._hasInterferenceWithMeeting(meeting, refMmeting)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    _hasFreeRoomForMeeting(meeting, rooms) {
+        let freeRoomId = 0;
+        for (const room of rooms) {
+            if (!this._hasInterferenceWithRoom(meeting, room)) {
+                return freeRoomId;
+            }
+            freeRoomId++;
+        }
+        return null;
+    }
+    _extendRoomMeetingTime(roomId, rooms, meeting) {
+        if (roomId !== null) {
+            rooms[roomId].push(meeting);
+        }
+        else {
+            rooms.push([meeting]);
+        }
+        return rooms;
+    }
+}
+let greeter = new Greeter("world");
+// _.times(4, () => console.log(greeter.greet()));
+let meetings = [[1, 3], [2, 4], [2, 5], [3, 4], [3, 6], [4, 5], [5, 6]];
+let rooms = greeter.arrangeRooms(meetings);
+console.log("\nMeetings: ", meetings, "\narranged rooms: ", rooms, "\nnumber of rooms: ", rooms.length);
+meetings = [[2, 10], [3, 4], [6, 9]];
+rooms = greeter.arrangeRooms(meetings);
+console.log("\nMeetings: ", meetings, "\narranged rooms: ", rooms, "\nnumber of rooms: ", rooms.length);
